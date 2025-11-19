bf6cf36af71837196eadbe47da4efce83016ecee
rara <inmenzora@omsc.edu.ph>
2025-11-19 14:04:10 +0800

    owlsome login, registration, and dashboard

---
+++ db_connect.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root"; // Default user
+$password = ""; // Default password is empty
+$database = "mamburao_db"; //Name of your database
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";

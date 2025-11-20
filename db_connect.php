8ee888bad2a4e4f31c5bfb247f54e9e0b277ac93
rara <inmenzora@omsc.edu.ph>
2025-11-20 11:25:48 +0800

    Update connect

--- db_connect.php
+++ db_connect.php
@@ -12,3 +12,12 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+
+//or you can also do this:
+// $conn = mysqli_connect("localhost", "root", "", "mamburao_db");
+
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
+// echo "Connected!";
